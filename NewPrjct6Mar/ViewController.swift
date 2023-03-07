ae3b727558f8f5c8595742113ecd00332ca89a0e
Techvantage <Ram.Kumar@techvantagesystems.com>
2023-03-07 17:04:51 +0530

    Initial Commit

---
+++ NewPrjct6Mar/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  NewPrjct6Mar
+//
+//  Created by TVPC000013 on 06/03/23.
+//
+
+import UIKit
+import NewFramwrk6Mar
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginBtn_Tapped(_ sender: UIButton){
+        present(LoginViewController(), animated: true, completion: nil)
+    }
+
+
+}
+
